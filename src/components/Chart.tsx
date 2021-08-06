b4e1eafec0511c1a874f5f3064df0b7c07406591
Dani Gasp <danigaspariam@gmail.com>
2021-08-06 10:34:01 -0300

    estilizacao do grafico maior em andamento

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import React, { useContext } from 'react';
+import { AreaChart, Area, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { makeStyles } from '@material-ui/core';
+import { StoreContext } from '..';
+import { observer } from 'mobx-react-lite';
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    backgroundColor: '#101010',
+    borderRadius: '8px',
+  },
+}));
 
 const data = [
   {
@@ -39,28 +49,43 @@ const data = [
   },
 ];
 
-export default function Chart() {
+const Chart = observer(() => {
+  const classes = useStyles();
+  const store = useContext(StoreContext);
+  const { account } = store;
   return (
-    <ResponsiveContainer width="100%" aspect={3}>
-      <AreaChart
-        width={500}
-        height={400}
-        data={data}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-
-        <Area dataKey="crvRenWBTC" stackId="1" stroke="#9E8EFF" strokeWidth="4" />
-        <Area dataKey="BadgerwBTC" stackId="1" stroke="#F2A627" strokeWidth="4" />
-      </AreaChart>
-    </ResponsiveContainer>
+    <div className={classes.box}>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorcrv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.9} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0.06} />
+            </linearGradient>
+            <linearGradient id="colorbad" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.9} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0.06} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Legend />
+          <Tooltip cursor={{ strokeDasharray: '4 4', stroke: '#aaaa' }} />
+          <Area dataKey="crvRenWBTC" stackId="1" stroke="#9E8EFF" strokeWidth="4" fill="url(#colorcrv)" />
+          <Area dataKey="BadgerwBTC" stackId="1" stroke="#F2A627" strokeWidth="4" fill="url(#colorbad)" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
   );
-}
+});
+export default Chart;
