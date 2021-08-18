d2fa3f3b1144f5e99f9760d9ac5d7f42fdfddb7b
Luane Caroline <luanecrln@gmail.com>
2021-08-18 11:24:14 -0300

    test

--- src/components/VaultsScreen.tsx
+++ src/components/VaultsScreen.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import { makeStyles, Box, Typography, Checkbox } from '@material-ui/core';
+import { makeStyles, Box, Checkbox } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
+
 const useStyles = makeStyles((theme) => ({
   vaultContainer: {
+    width: 935,
+    height: 496,
     background: '#222222',
+    border: '1px solid #111111',
+    borderRadius: 8,
   },
-
   headerVault: {
     background: '#222222',
+    display: 'flex',
+  },
+  alignbuttom: {
+    color: 'red',
+    fontSize: 13,
+    position: 'absolute',
+    width: '86%',
+    display: 'flex',
+    alignItems: 'space-between',
+    justifyContent: 'space-between',
   },
 }));
+
 const VaultsScreen = () => {
   const classes = useStyles();
   return (
     <Box component="div" className={classes.vaultContainer}>
       <Box component="header" className={classes.headerVault}>
         <Box>
-          <Checkbox />
-          <Typography variant="h6">all set vaults</Typography>
+          <div className={classes.alignbuttom}>
+            <Checkbox />
+            <CloseIcon style={{}} />
+          </div>
         </Box>
       </Box>
-      <CloseIcon />
     </Box>
   );
 };
